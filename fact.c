a8b25c7fd71f134c43e39a389043ba93c8035806
vishnu <vishnu@gmail.com>
2021-11-29 22:38:04 +0530

    makefile demo

---
+++ fact.c
@@ -0,0 +1,14 @@
+void fact() {
+#include<stdio.h>
+void main()
+{
+ int i,fact=1,number;
+ printf("Enter a number: ");
+  scanf("%d",&number);
+    for(i=1;i<=number;i++){
+      fact=fact*i;
+  }
+  printf("Factorial of %d is: %d",number,fact);
+//return 0;
+}
+}
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+main()
+{
+fact();
+big3();
+}
